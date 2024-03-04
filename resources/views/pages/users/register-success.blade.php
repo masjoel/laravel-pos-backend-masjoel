53a4bc58a9a3958052d5c30a04740728e48030dd
Masjoel <masjoel@gmail.com>
2024-03-04 16:45:37 +0700

    redirect html

--- resources/views/pages/users/register-success.blade.php
+++ resources/views/pages/users/register-success.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; URL=https://kasir.tokopojok.com/konfirmasi-success.html" />
     <title>Konfirmasi Email</title>
 </head>
 <body>
-    <h1 style="text-align: center; font-family: Arial, Helvetica, sans-serif; margin-top: 50px">Akun Anda sudah aktif</h1>
-    <p style="text-align: center; font-family: Arial, Helvetica, sans-serif;">silahkan login di aplikasi Kasir</p>
+    {{-- <h1 style="text-align: center; font-family: Arial, Helvetica, sans-serif; margin-top: 50px">Akun Anda sudah aktif</h1>
+    <p style="text-align: center; font-family: Arial, Helvetica, sans-serif;">silahkan login di aplikasi Kasir</p> --}}
 </body>
 </html>
