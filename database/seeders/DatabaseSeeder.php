5677af1722258735ff78c50141c18cc6e045dc0b
Masjoel <masjoel@gmail.com>
2024-09-20 06:46:02 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
         
         User::factory()->create([
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call([
-            ProductSeeder::class,
+            // ProductSeeder::class,
         ]);
     }
 }
