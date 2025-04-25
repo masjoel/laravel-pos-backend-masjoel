a9737c0eac2c9b0f6fda306658375d0fa4ac13ba
Masjoel <masjoel@gmail.com>
2025-04-25 20:50:15 +0700

    update crud users

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+        Copyright &copy; {{ date('Y') }} <div class="bullet"></div> 
     </div>
     <div class="footer-right">
         2.3.0
