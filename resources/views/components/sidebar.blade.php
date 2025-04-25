4af43c5b6f4c18b939ecf20ac8095b8d5fe0666e
Masjoel <masjoel@gmail.com>
2025-04-25 20:59:51 +0700

    update menu

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,11 +8,15 @@
         </div>
         <ul class="sidebar-menu">
             <li class="nav-item">
-                <a href="{{ route('home') }}" class="nav-link {{ Request::is('dashboard*') ? 'active' : '' }}"><i class="fas fa-dashboard"></i><span>Dashboard</span></a>
+                <a href="{{ route('home') }}" class="nav-link {{ Request::is('dashboard*') ? 'active' : '' }}"><i
+                        class="fas fa-dashboard"></i><span>Dashboard</span></a>
             </li>
-            <li class="nav-item">
-                <a href="{{ route('user.index') }}" class="nav-link {{ Request::is('user') ? 'active' : '' }}"><i class="fas fa-users"></i><span>Users</span></a>
-            </li>
+            @if (auth()->user()->roles == 'admin' || auth()->user()->roles == 'reseller')
+                <li class="nav-item">
+                    <a href="{{ route('user.index') }}" class="nav-link {{ Request::is('user') ? 'active' : '' }}"><i
+                            class="fas fa-users"></i><span>Users</span></a>
+                </li>
+            @endif
             {{-- <li class="nav-item">
                 <a href="{{ route('product.index') }}" class="nav-link {{ Request::is('product') ? 'active' : '' }}"><i class="fas fa-gift"></i><span>Products</span></a>
             </li>
