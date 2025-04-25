b834aad517546fbdadcb4780a18ee9dfe0968f96
Masjoel <masjoel@gmail.com>
2025-04-25 20:52:01 +0700

    update crud users

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,10 +15,10 @@
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-title">Logged in 5 min ago</div>
-                <a href="#" class="dropdown-item has-icon">
+                {{-- <a href="#" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
-                </a>
-                <div class="dropdown-divider"></div>
+                </a> --}}
+                {{-- <div class="dropdown-divider"></div> --}}
                 <a href="#" class="dropdown-item has-icon text-danger"
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
